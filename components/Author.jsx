64bb21ce4c048c8df1f2a7b8596a9cc215b96b8c
Seundede <omadoyedede0@gmail.com>
2022-06-29 13:15:25 +0100

    author

--- components/Author.jsx
+++ components/Author.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
-
-const Author = () => {
+import React from "react";
+import Image from "next/image";
+const Author = ({ author }) => {
+ 
   return (
-    <div>Author</div>
-  )
-}
+    <div className="text-center mt-20 mb-8 p-12 relative rounded-lg  bg-opacity-20">
+      <div className="absolute left-0 right-0 -top-14">
+        <Image
+          unoptimized
+          alt={author.name}
+          height="100px"
+          width="100px"
+          className="align-middle rounded-full"
+          src={author.picture.url}
+        />
+      </div>
+      <h3 className=" mt-4 mb-4 text-xl font-bold">{author.name}</h3>
+      <p className=" text-ls">{author.bio}</p>
+    </div>
+  );
+};
 
-export default Author
+export default Author;
+
+{
+  /**    loader={grpahCMSImageLoader} */
+}
